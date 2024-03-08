168dc2e830709a7b67e8fe1da357611568d50a1a
안승우 <rio319@naver.com>
2024-03-09 01:24:14 +0900

    #3 feat: 공용 topbar 개발

--- presentation/src/main/java/io/tuttut/presentation/theme/Dimen.kt
+++ presentation/src/main/java/io/tuttut/presentation/theme/Dimen.kt
@@ -4,8 +4,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
-private val screenHorizontalPadding = 20.dp
-private val screenVerticalPadding = 30.dp
+val screenHorizontalPadding = 20.dp
+val screenVerticalPadding = 30.dp
 val buttonHeight = 60.dp
 
 fun Modifier.withScreenPadding(): Modifier {
