8cdae8938e01f2e8b39ab1f5589cc0c649215455
안승우 <rio319@naver.com>
2024-03-09 15:19:40 +0900

    #5 feat: 환영 text 가운데 정렬

--- presentation/src/main/java/io/tuttut/presentation/ui/screen/login/welcome/WelcomeScreen.kt
+++ presentation/src/main/java/io/tuttut/presentation/ui/screen/login/welcome/WelcomeScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import io.tuttut.presentation.R
@@ -42,7 +43,8 @@ internal fun WelcomeScreen(modifier: Modifier, onNext: () -> Unit) {
         Text(
             text = stringResource(id = R.string.welcome),
             style = MaterialTheme.typography.bodyLarge,
-            lineHeight = 30.sp
+            lineHeight = 30.sp,
+            textAlign = TextAlign.Center
         )
         Spacer(modifier = Modifier.weight(1f))
         TutTutButton(
