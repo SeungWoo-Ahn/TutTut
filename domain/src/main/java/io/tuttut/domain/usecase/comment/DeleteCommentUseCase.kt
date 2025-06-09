f35ba933193204e6cc7ac52b3d25e8403cbfecd4
안승우 <rio319@naver.com>
2025-06-09 22:19:55 +0900

    #32 refactor: comment 관련 UseCase Credential 방식으로 변경

--- domain/src/main/java/io/tuttut/domain/usecase/comment/DeleteCommentUseCase.kt
+++ domain/src/main/java/io/tuttut/domain/usecase/comment/DeleteCommentUseCase.kt
@@ -1,9 +1,9 @@
 package io.tuttut.domain.usecase.comment
 
-import io.tuttut.domain.exception.ExceptionBoundary
 import io.tuttut.domain.model.comment.DeleteCommentRequest
 import io.tuttut.domain.repository.CommentRepository
 import io.tuttut.domain.repository.PreferenceRepository
+import io.tuttut.domain.util.runCatchingExceptCancel
 import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 
@@ -11,11 +11,10 @@ class DeleteCommentUseCase @Inject constructor(
     private val commentRepository: CommentRepository,
     private val preferenceRepository: PreferenceRepository,
 ) {
-    suspend operator fun invoke(diaryId: String, commentId: String): Result<Unit> = runCatching {
-        val gardenId = preferenceRepository.getGardenIdFlow().first()
-            ?: throw ExceptionBoundary.UnAuthenticated()
+    suspend operator fun invoke(diaryId: String, commentId: String): Result<Unit> = runCatchingExceptCancel {
+        val credential = preferenceRepository.getCredentialFlow().first()
         val deleteCommentRequest = DeleteCommentRequest(
-            gardenId = gardenId,
+            gardenId = credential.gardenId,
             diaryId = diaryId,
             commentId = commentId,
         )
