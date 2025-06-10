03d12adb0881a10b013705cf2d52ba5c2efc7edb
안승우 <rio319@naver.com>
2025-06-10 19:00:42 +0900

    #32 refactor: 불필요한 모델 제거

--- data/src/main/java/io/tuttut/data/model/response/Result.kt
+++
@@ -1,8 +0,0 @@
-package io.tuttut.data.model.response
-
-sealed interface Result<out T> {
-    data class Success<T>(val data: T) : Result<T>
-    data class Error(val exception: Throwable) : Result<Nothing>
-    data object NotFound : Result<Nothing>
-    data object Loading : Result<Nothing>
-}
