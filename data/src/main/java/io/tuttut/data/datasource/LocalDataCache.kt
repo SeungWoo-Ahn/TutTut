033422eb8e2dc5283c63012cde0cec81ba7d9797
안승우 <rio319@naver.com>
2025-06-11 14:57:43 +0900

    #32 feat: Local Data Cache 구현

---
+++ data/src/main/java/io/tuttut/data/datasource/LocalDataCache.kt
@@ -0,0 +1,27 @@
+package io.tuttut.data.datasource
+
+import io.tuttut.domain.model.cropsInfo.CropsInfo
+import io.tuttut.domain.model.cropsInfo.CropsKey
+import io.tuttut.domain.model.user.UpdateUserRequest
+import io.tuttut.domain.model.user.User
+import kotlinx.coroutines.flow.StateFlow
+
+interface LocalDataCache {
+    val currentUser: StateFlow<User?>
+
+    val cropsInfoList: StateFlow<List<CropsInfo>>
+
+    fun setCurrentUser(user: User)
+
+    fun updateCurrentUser(updateUserRequest: UpdateUserRequest)
+
+    fun getGardenUserById(id: String): User?
+
+    fun setGardenUser(user: User)
+
+    fun setCropsInfoList(cropsInfoList: List<CropsInfo>)
+
+    fun getCropsInfoByKey(cropsKey: CropsKey): CropsInfo?
+
+    fun setCropsInfo(cropsInfo: CropsInfo)
+}
---
+++ data/src/main/java/io/tuttut/data/datasource/LocalDataCacheImpl.kt
@@ -0,0 +1,60 @@
+package io.tuttut.data.datasource
+
+import io.tuttut.domain.model.cropsInfo.CropsInfo
+import io.tuttut.domain.model.cropsInfo.CropsKey
+import io.tuttut.domain.model.user.UpdateUserRequest
+import io.tuttut.domain.model.user.User
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import java.util.concurrent.ConcurrentHashMap
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class LocalDataCacheImpl @Inject constructor() : LocalDataCache  {
+    private val _currentUser = MutableStateFlow<User?>(null)
+    override val currentUser: StateFlow<User?> get() = _currentUser
+
+    private val _cropsInfoList = MutableStateFlow<List<CropsInfo>>(emptyList())
+    override val cropsInfoList: StateFlow<List<CropsInfo>> get() = _cropsInfoList
+
+    private val gardenUserMap = ConcurrentHashMap<String, User>()
+    private val cropsInfoMap = ConcurrentHashMap<CropsKey, CropsInfo>()
+
+    override fun setCurrentUser(user: User) {
+        _currentUser.update { user }
+    }
+
+    override fun updateCurrentUser(updateUserRequest: UpdateUserRequest) {
+        _currentUser.update {
+            _currentUser.value?.copy(
+                name = updateUserRequest.name,
+                profile = updateUserRequest.profile
+            )
+        }
+    }
+
+    override fun getGardenUserById(id: String): User? {
+        return gardenUserMap[id]
+    }
+
+    override fun setGardenUser(user: User) {
+        gardenUserMap[user.id] = user
+    }
+
+    override fun setCropsInfoList(cropsInfoList: List<CropsInfo>) {
+        _cropsInfoList.update { cropsInfoList }
+        cropsInfoList.forEach {
+            cropsInfoMap[it.key] = it
+        }
+    }
+
+    override fun getCropsInfoByKey(cropsKey: CropsKey): CropsInfo? {
+        return cropsInfoMap[cropsKey]
+    }
+
+    override fun setCropsInfo(cropsInfo: CropsInfo) {
+        cropsInfoMap[cropsInfo.key] = cropsInfo
+    }
+}
