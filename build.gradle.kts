aff1dbbd3138f508c5bfb6f090627b6dc7bfcffa
안승우 <rio319@naver.com>
2025-07-04 11:37:18 +0900

    #36 feat: Kotlin Parcelize 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,4 +9,5 @@ plugins {
     alias(libs.plugins.google.service) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.parcelize) apply false
 }
--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
     alias(libs.plugins.ksp)
     alias(libs.plugins.google.service)
     alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.parcelize)
 }
 
 android {
